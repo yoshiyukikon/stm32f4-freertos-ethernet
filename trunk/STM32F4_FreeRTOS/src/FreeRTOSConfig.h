c0194566506e671d281aec1f97f9ccc4367b0909
dombog@gmail.com <dombog@gmail.com@eaa454d5-e75b-64a6-bff6-680e38986867>
2012-04-23 14:12:31 +0000

    Update STM32 Lib to 1.0.0 to 1.0.1 + set HCLK to 120Mhz

--- trunk/STM32F4_FreeRTOS/src/FreeRTOSConfig.h
+++ trunk/STM32F4_FreeRTOS/src/FreeRTOSConfig.h
@@ -78,7 +78,7 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				0
-#define configCPU_CLOCK_HZ				( 168000000UL )
+#define configCPU_CLOCK_HZ				( 120000000UL )
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 5 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 200 )
--- trunk/STM32F4_FreeRTOS/src/main.h
+++ trunk/STM32F4_FreeRTOS/src/main.h
@@ -1,10 +1,10 @@
 /**
   ******************************************************************************
-  * @file    Project/STM32F4xx_StdPeriph_Templates/stm32f4xx_it.h 
+  * @file    Project/STM32F4xx_StdPeriph_Templates/main.h 
   * @author  MCD Application Team
   * @version V1.0.1
   * @date    13-April-2012
-  * @brief   This file contains the headers of the interrupt handlers.
+  * @brief   Header for main.c module
   ******************************************************************************
   * @attention
   *
@@ -24,14 +24,10 @@
   *
   ******************************************************************************
   */
-
+  
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F4xx_IT_H
-#define __STM32F4xx_IT_H
-
-#ifdef __cplusplus
- extern "C" {
-#endif 
+#ifndef __MAIN_H
+#define __MAIN_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
@@ -41,20 +37,6 @@
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void MemManage_Handler(void);
-void BusFault_Handler(void);
-void UsageFault_Handler(void);
-void SVC_Handler(void);
-void DebugMon_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __STM32F4xx_IT_H */
+#endif /* __MAIN_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
